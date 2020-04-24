93bb3c603f08e67339be68fc5536a9d0defb3f6f
Syed Ali Rizvi <syedalirizvi@outlook.com>
2020-04-23 21:05:35 -0400

    Part 6 Lecture

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'ZeeshanYasin',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 22nd, 2020',
-    },
-    {
-        'author': 'AshalAbbas',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 23rd, 2020',
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
